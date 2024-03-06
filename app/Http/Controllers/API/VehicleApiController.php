c6ce4b67b7e0c4eac3d9eb84d9a55a687b5c47f7
suryaalagar <120779704+suryaalagar@users.noreply.github.com>
2024-03-06 17:42:24 +0530

    Trip_complete_with_vehicle_status

--- app/Http/Controllers/API/VehicleApiController.php
+++ app/Http/Controllers/API/VehicleApiController.php
@@ -3,29 +3,59 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\TripplanReport;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class VehicleApiController extends Controller
 {
+    // public function vehicle_status(Request $request)
+    // {
+    //     $get_data = $request->all();
+    //     $device_imei = $get_data['device_imei'];
+    //     date_default_timezone_set('Asia/Kolkata');
+    //     $current_time = date("Y-m-d H:i:s");
+    //     $query = DB::table('live_data')
+    //         ->where('deviceimei', '=', $device_imei)
+    //         ->select(
+    //             'vehicle_name',
+    //             'deviceimei',
+    //             'lattitute',
+    //             'longitute',
+    //             'ignition',
+    //             'speed',
+    //             'device_updatedtime',
+    //             DB::raw('TIMESTAMPDIFF(MINUTE, device_updatedtime, "' . $current_time . '") AS update_time')
+    //         )->first();
+    //     return $query;
+    // }
     public function vehicle_status(Request $request)
     {
         $get_data = $request->all();
         $device_imei = $get_data['device_imei'];
         date_default_timezone_set('Asia/Kolkata');
         $current_time = date("Y-m-d H:i:s");
-        $query = DB::table('live_data')
-            ->where('deviceimei', '=', $device_imei)
+        $query = 
+            TripplanReport::join('live_data AS l','l.deviceimei', '=', 'tripplan_reports.device_imei')
+            ->where('tripplan_reports.device_imei', '=', $device_imei)
             ->select(
-                'vehicle_name',
-                'deviceimei',
-                'lattitute',
-                'longitute',
-                'ignition',
-                'speed',
-                'device_updatedtime',
-                DB::raw('TIMESTAMPDIFF(MINUTE, device_updatedtime, "' . $current_time . '") AS update_time')
-            )->first();
+                'l.vehicle_name',
+                'l.deviceimei',
+                'l.lattitute',
+                'l.longitute',
+                'l.ignition',
+                'l.speed',
+                'l.angle',
+                DB::raw('TIMESTAMPDIFF(MINUTE, l.device_updatedtime, "' . $current_time . '") AS update_time'),
+                DB::raw('(CASE 
+                WHEN tripplan_reports.status = 1 THEN "In Hub"
+                WHEN tripplan_reports.status = 2 THEN "In Processing"
+                ELSE "Complete"
+                END) AS trip_status')
+            )
+            ->orderBy('tripplan_reports.trip_id', 'DESC')
+            ->first();
+            
         return $query;
     }
 }
